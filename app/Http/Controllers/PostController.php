069160df834e4a222ffd4b8df91bcbf54d2264ad
Ohtaki-endless <orora_togiants@yahoo.co.jp>
2021-09-12 16:29:17 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,14 +7,25 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
+        /**
+     * Post一覧を表示する
+     *
+     * @param Post Postモデル
+     * @return array Postモデルリスト
+     */
     public function index(Post $post)
     {
         return view('index')->with(['posts' => $post->getPaginateByLimit(5)]);  
+    }
+
+    /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+    */
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
     }
 }
